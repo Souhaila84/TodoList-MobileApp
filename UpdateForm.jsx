ffa9c8c3f53736a3e8d18e5119ca1f0dba653bdf
Souhaila_Moumane <92530859+Souhaila84@users.noreply.github.com>
2023-03-26 19:33:25 +0200

    Delete UpdateForm.jsx

--- UpdateForm.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function UpdateForm(updateData, changeTask, updateTask, cancelUpdate){
-    return(
-        <div className="UpdateForm">
-            <button id="cancel" className="btn btn-success" hidden>Cancel</button>
-        </div>
-    );
-}
